2a87136c93bc4dc64c8120775f7bfd34a7c2496c
parthvshah <parthvshah@gmail.com>
2019-04-25 22:52:52 +0530

    Delete button for distributors

--- php/deleteDistributor.php
+++ php/deleteDistributor.php
@@ -14,5 +14,5 @@ $db_connection = pg_connect("host=localhost dbname=test0 user=postgres password=
 $sql = sprintf("DELETE FROM Distributors WHERE DistributorId='%s'",$id);
 $result = pg_query($db_connection, $sql);
 
-echo '<script>window.location.replace("/index.html");</script>';
+echo '<script>window.location.replace("/distributors.html");</script>';
 ?>
