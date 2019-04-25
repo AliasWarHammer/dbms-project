f83f328be0e075706517273909489391e24111eb
parthvshah <parthvshah@gmail.com>
2019-04-25 22:48:48 +0530

    Delete button for products

--- php/deleteProducts.php
+++ php/deleteProducts.php
@@ -6,7 +6,6 @@ if(empty($_POST['deleteID']))
    }
 
 $id = strip_tags(htmlspecialchars($_POST['deleteID']));
-
 $db_connection = pg_connect("host=localhost dbname=test0 user=postgres password=tt");
 $idlaptop = $id;
 $idphone = $id;
@@ -17,9 +16,8 @@ $result1 = pg_query($db_connection, $sql1);
 $sql2 = sprintf("DELETE FROM Phones WHERE ProductId = '%s'",$idphone);
 $result2 = pg_query($db_connection, $sql2);
 
-
 $sql = sprintf("DELETE FROM Products WHERE ProductId = '%s'",$id);
 $result = pg_query($db_connection, $sql);
 
-echo '<script>window.location.replace("/index.html");</script>';
+echo '<script>window.location.replace("/products.html");</script>';
 ?>
